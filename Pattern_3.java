e3e3751af379bec2a610ad4041725e8e11f9c184
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-08-21 23:41:11 +0530

    Pattern_3

---
+++ Pattern_3.java
@@ -0,0 +1,18 @@
+public class Pattern_3 {
+    public static void main(String[] args) {
+        int n=5;
+        int space=n-1;
+        int star=1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < space; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < star; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            space--;
+            star++;
+        }
+    }
+}
