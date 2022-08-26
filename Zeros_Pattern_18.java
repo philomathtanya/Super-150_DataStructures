85df00339be4803e9bb1e44af47ec910c35db688
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-08-26 22:35:20 +0530

    Add files via upload

---
+++ Zeros_Pattern_18.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Zeros_Pattern_18 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=i; j++) {
+                if(j==1 || j==i)
+                {
+                    System.out.print(i+" ");
+                }
+                else
+                    System.out.print("0"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
