8463507ecbde7f8b1dcf1619afe138fcb5364fc5
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-09-29 20:59:58 +0530

    Permutation_String

---
+++ Permutation_String.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Permutation_String {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String str="abc";
+        printanswer(str,"");
+    }
+    public static void printanswer(String ques,String ans){
+        if(ques.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        for (int i = 0; i < ques.length(); i++) {
+            char ch=ques.charAt(i);
+            String s1=ques.substring(0,i);
+            String s2=ques.substring(i+1);
+            printanswer(s1+s2,ans+ch);
+
+        }
+    }
+}
