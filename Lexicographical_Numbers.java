d5af497a1de4bd257ca556de76415b5e8935580c
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-10-14 22:55:49 +0530

    Lexicographical_Numbers

---
+++ Lexicographical_Numbers.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class Lexicographical_Numbers {
+    public static void main(String[] args) {
+        int n=1000;
+        lexicoCounting(0,n);
+    }
+//    public static List<Integer> lexicalOrder(int n) {
+//        lexicoCounting(0,n);
+//
+//    }
+    public static void lexicoCounting(int curr,int n){
+        if(curr>n){
+            return;
+        }
+        System.out.println(curr);
+        int i=0;
+        if(curr==0){
+        i=1;
+        }
+        for (; i <=9; i++) {
+            lexicoCounting((curr*10)+i,n);
+        }
+    }
+}
