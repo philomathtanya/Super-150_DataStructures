4b93dfd5da68580f740f81d66835ea9b1beea8dc
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-10-15 23:46:21 +0530

    Queen_Permutation

---
+++ Queen_Permutation.java
@@ -0,0 +1,21 @@
+public class Queen_Permutation {
+    public static void main(String[] args) {
+        int n=4;
+        int tq=2;
+        boolean [] board=new boolean[n];
+        printanswer(board,tq,0,"");
+    }
+    public static void printanswer(boolean[] board , int tq ,int qpsf ,String ans){
+        if(qpsf==tq){
+            System.out.println(ans);
+            return;
+        }
+        for (int i = 0; i < board.length; i++) {
+            if(board[i]==false){
+                board[i]=true;
+                printanswer(board,tq,qpsf+1,ans+"B"+i+"Q"+qpsf);
+                board[i]=false;
+            }
+        }
+    }
+}
