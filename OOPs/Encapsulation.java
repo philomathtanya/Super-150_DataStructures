fb83f6372d3b8ca21b6e7ba5eb672974460574e7
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-11-18 23:20:42 +0530

    encap

---
+++ OOPs/Encapsulation.java
@@ -0,0 +1,10 @@
+//import Student_Oops_Example;
+public class Encapsulation {
+    public static void main(String[] args) throws Exception {
+        var obj = new Student_Oops_Example();
+        obj.setAge(-52);
+        obj.setName("Ritesh");
+        System.out.println(obj.getName());
+        System.out.println(obj.getAge());
+    }
+}
