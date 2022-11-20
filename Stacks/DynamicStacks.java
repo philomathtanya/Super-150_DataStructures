08a5560ae8f5c6a835f15cffd2155947eccba24e
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-11-20 22:49:41 +0530

    update

--- Stacks/DynamicStacks.java
+++ Stacks/DynamicStacks.java
@@ -1,11 +1,29 @@
-package Stacks;
 
-public class DynamicStacks extends Stacks_basic{
-    public static void main(String[] args) {
-        @Override
-       public void push(int item){
+public class DynamicStack extends Stack_basic{
 
-        }
+	@Override
+	public void push(int item) throws Exception {
+		if (this.isfull()) {
+			int[] a = new int[2 * arr.length];
+			for (int i = 0; i < arr.length; i++) {
+				a[i] = arr[i];
+			}
+			arr = a;
 
-    }
+		}
+		super.push(item);
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		DynamicStack ds = new DynamicStack();
+		ds.push(10);
+		ds.push(20);
+		ds.push(30);
+		ds.push(40);
+		ds.push(50);
+		ds.push(60);
+		ds.display();
+	}
+
 }
