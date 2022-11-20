cb5ba43123be86ffa9597b39c9feca84098b7f43
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-11-20 22:46:57 +0530

    DynamicStacks

---
+++ Stacks/DynamicStacks.java
@@ -0,0 +1,11 @@
+package Stacks;
+
+public class DynamicStacks extends Stacks_basic{
+    public static void main(String[] args) {
+        @Override
+       public void push(int item){
+
+        }
+
+    }
+}
