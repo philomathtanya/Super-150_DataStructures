479c727dc37ca70fd560245c8653e0f82df4d371
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2023-01-12 21:48:32 +0530

    Reverse_LinkedList

---
+++ LinkedList/Reverse_LinkedList.java
@@ -0,0 +1,27 @@
+class Reverse_LinkedList {
+        class ListNode {
+            int val;
+            ListNode next;
+
+            ListNode(int x) {
+                val = x;
+                next = null;
+            }
+        }
+
+        class Solution {
+            public ListNode reverseList(ListNode head) {
+                ListNode curr = head;
+                ListNode prev = null;
+                while (curr != null) {
+                    ListNode ahead = curr.next;
+                    curr.next = prev;
+                    prev = curr;
+                    curr = ahead;
+                }
+                return prev;
+
+            }
+        }
+
+}
