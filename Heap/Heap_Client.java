0129b62a1ea0517bcb6dbe9139a7e3021052b18d
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2023-03-17 21:54:19 +0530

    Heap_Client

---
+++ Heap/Heap_Client.java
@@ -0,0 +1,23 @@
+package Heap;
+public class Heap_Client {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Heap_Create hp = new Heap_Create();
+        hp.add(10);
+        hp.add(20);
+        hp.add(30);
+        hp.add(5);
+        hp.add(7);
+        hp.add(3);
+        hp.add(2);
+        hp.add(-10);
+        hp.add(-20);
+        System.out.println(hp.getmin());
+        hp.display();
+        System.out.println(hp.remove());
+        hp.display();
+        System.out.println(hp.remove());
+        hp.display();
+    }
+}
