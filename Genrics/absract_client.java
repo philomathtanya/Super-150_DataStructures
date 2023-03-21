607ed4e7a94ad09787916545f1529afb27fb15bc
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2023-03-21 20:56:52 +0530

    Add files via upload

---
+++ Genrics/absract_client.java
@@ -0,0 +1,12 @@
+
+public class absract_client{
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+//        abstract_demo ab = new  abstract_demo();
+//        ab.fun();
+//        ab.fun1();
+        //we can not make obj
+    }
+
+}
