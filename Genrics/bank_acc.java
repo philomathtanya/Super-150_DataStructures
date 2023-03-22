5902f149cfb8b98c7f4213f0bdd6a55c1df38e8e
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2023-03-22 22:42:45 +0530

    bank_acc

---
+++ Genrics/bank_acc.java
@@ -0,0 +1,12 @@
+//public class bank_acc {
+//}
+
+public abstract class bank_acc {
+
+    public  abstract boolean payment() ;
+
+    public  int accountbalnec() {
+        return 1000;
+    }
+
+}
