cfd1ef8292946b326c4fcd3b28ee004a31425b57
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2023-03-23 22:47:19 +0530

    Generics_Client

---
+++ Genrics/Generics_Client.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+
+public class Generics_Client {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Generics_Demo<String, Integer> g = new Generics_Demo<>();
+        Generics_Demo<String, Boolean> g1 = new Generics_Demo<>();
+
+
+   
+    }
+
+}
