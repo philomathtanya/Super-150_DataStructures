3fd92d793cfe6ddae0539910a688530f46ee5db1
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2023-03-24 21:55:30 +0530

    payment_method

---
+++ Genrics/payment_method.java
@@ -0,0 +1,15 @@
+
+public class payment_method extends bank_acc {
+
+    @Override
+    public boolean payment() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public static void main(String[] args) {
+        payment_method pp = new payment_method();
+
+    }
+
+}
