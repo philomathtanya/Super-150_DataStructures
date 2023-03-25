6755e7916d2ccdd2e458b3a520fc51577612f670
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2023-03-25 23:13:27 +0530

    Sort_game

---
+++ Comparater Camparable/Sort_game.java
@@ -0,0 +1,48 @@
+
+
+import java.util.*;
+
+public class Sort_game {
+
+    String name;
+    int salary;
+
+    public Sort_game(String name, int salary) {
+        // TODO Auto-generated constructor stub
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int n = sc.nextInt();
+        Sort_game[] emp = new Sort_game[n];
+        for (int i = 0; i < emp.length; i++) {
+            String str = sc.next();
+            int s = sc.nextInt();
+            emp[i] = new Sort_game(str, s);
+
+        }
+
+        Arrays.sort(emp,new Comparator<Sort_game>() {
+
+            @Override
+            public int compare(Sort_game o1, Sort_game o2) {
+                if(o1.salary==o2.salary) {
+                    return o1.name.compareTo(o2.name);
+                }
+                else {
+                    return o2.salary-o1.salary;
+                }
+            }
+        });
+
+        for (int i = 0; i < emp.length; i++) {
+            if (emp[i].salary >= x) {
+                System.out.println(emp[i].name + " " + emp[i].salary);
+            }
+        }
+
+    }
+}
